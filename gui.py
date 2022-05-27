8517cfbf53f324ff40fffad71bc1e4671bb97440
Islam AlaaEddin <islamalaaeddin1998@gmail.com>
2022-05-27 16:51:28 +0200

    Adding streams list box

--- gui.py
+++ gui.py
@@ -26,5 +26,17 @@ img = ImageTk.PhotoImage(Image.open("jerry.jpg").resize((196, 196)))
 panel = Label(frame, image=img, anchor="w")
 panel.grid(row=2, column=0, padx=8, pady=8)
 
+## streams list box
+streams_listbox = tkinter.Listbox(frame)
+
+for i in range(100):
+    streams_listbox.insert(str(i), ' ------------------- ' + str(i + 1))
+
+streams_listbox.grid(row=2, column=1, padx=8, pady=8, sticky='EW')
+
+# myscroll = Scrollbar(frame, orient='vertical')
+# myscroll.grid(row=2, column=1, sticky='ns')
+# myscroll.config(command = streams_listbox.yview)
+
 root.title("Youtube Downloader")
 root.mainloop()
