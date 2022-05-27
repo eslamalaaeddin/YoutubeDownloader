6e89c47161c5d647d9096856af967ef4485db520
Islam AlaaEddin <islamalaaeddin1998@gmail.com>
2022-05-27 16:49:44 +0200

    Adding url label

--- gui.py
+++ gui.py
@@ -8,6 +8,9 @@ root.geometry("720x400")
 frame = tkinter.Frame(root)
 frame.pack()
 
+## url label
+url_label = Label(frame, text="Video URL")
+url_label.grid(row=0, column=0, padx=8, pady=8)
 
 root.title("Youtube Downloader")
 root.mainloop()
