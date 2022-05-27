252ed9570aed960e5b4ff45f4f0527c64b2986fa
Islam AlaaEddin <islamalaaeddin1998@gmail.com>
2022-05-28 00:13:27 +0200

    Tool V1

--- gui.py
+++ gui.py
@@ -1,25 +1,165 @@
 import tkinter
-from tkinter.ttk import Label, Entry, Button, Scrollbar
-
+from tkinter import CENTER, HORIZONTAL
+from tkinter.ttk import Label, Entry, Button, Progressbar
+import requests
+from io import BytesIO
+import pytube
 from PIL import ImageTk, Image
 
+import threading
+
+file_size = 0
+
+# import commands
+from pytube import YouTube
+
+
+def open_popup(text):
+    top = tkinter.Toplevel(root)
+    width = 320
+    height = 224
+    top.geometry(str(width) + "x" + str(height))
+    top.title("Info")
+    Label(top, text=text, font=('Mistral 16 bold'), justify='center').place(relx=0.5, rely=0.5, anchor=CENTER)
+
+
+def progress_Check(stream=None, chunk=None, remaining=0):
+    # global file_size
+    # percent = file_size - remaining + 1000000
+    #
+    # try:
+    #     # updates the progress bar
+    #     download_progress_bar.update()
+    # except:
+    #     # progress bar dont reach 100% so a little trick to make it 100
+    #     download_progress_bar.update()
+    print('something')
+
+
+def show_all_streams(url):
+    yt = None
+    try:
+        yt = YouTube(url=url, on_progress_callback=progress_Check(), on_complete_callback=None)
+
+    except Exception as e:
+        print(e)
+
+    return yt, yt.streams.filter(progressive=True).otf(False), yt.streams.filter(type='audio') if yt is not None else None
+
+
+def on_search_button_clicked():
+    url = url_entry.get()
+    global video_streams
+    global audio_streams
+    # yt, streams = commands.search_with_url(url)
+    yt, video_streams, audio_streams = show_all_streams(url)
+
+
+    if video_streams is not None:
+        for i, s in enumerate(video_streams):
+            print(s)
+            if s.type == 'video':
+                media_label = s.title + ' - ' + str(s.resolution) + ' - ' + str(s.fps) + 'fps' + ' - ' + str(
+                    round(s.filesize / 1000000, 2)) + 'MB'
+            else:
+                media_label = s.title + ' - ' + str(s.abr) + ' - ' + str(round(s.filesize / 1000000, 2)) + 'MB'
+            # print(s)
+            streams_listbox.insert(i, media_label + '|' + str(s.itag))
+
+    if audio_streams is not None:
+        for i, s in enumerate(audio_streams):
+            print(s)
+            media_label = s.title + ' - ' + str(s.abr) + ' - ' + str(round(s.filesize / 1000000, 2)) + 'MB'
+            # print(s)
+            streams_listbox.insert(i, media_label + '|' + str(s.itag))
+        # response = requests.get(yt.thumbnail_url)
+        # img = Image.open(BytesIO(response.content))
+        # img = ImageTk.PhotoImage(Image.open(BytesIO(response.content)).resize((196, 196)))
+
+        # img.show()
+
+        # panel = Label(frame, image=img, anchor="w")
+        # panel.grid(row=2, column=0, padx=8, pady=8)
+    else:
+        print('Error')
+
+
+def search_threaded():
+    global search_thread
+    search_thread = threading.Thread(target=on_search_button_clicked, daemon=True)
+    search_thread.start()
+
+
+def download_threaded():
+    threading.Thread(target=download_selected, daemon=True).start()
+
+
+def cancel_download():
+    print('Cancelling')
+
+
+def download_selected():
+    selected = streams_listbox.curselection()  # returns a tuple
+    if not selected:
+        info = "اختار حاجة أحملها لك الأول يا بغل".split(' ')
+        info.reverse()
+        info = ' '.join(info)
+        open_popup(info)
+
+    for idx in selected:
+        itag = streams_listbox.get(idx).rpartition('|')[-1]
+        print('Downloading ', idx, itag)
+        aud_streams = audio_streams.get_by_itag(itag)
+        vid_streams = video_streams.get_by_itag(itag)
+
+        if vid_streams is not None:
+            print(vid_streams.filesize)
+            vid_streams.download(
+                output_path=output_path_entry.get(),
+                filename_prefix=itag)
+            print('Download Completed')
+
+        if aud_streams is not None:
+            print(aud_streams.filesize)
+            aud_streams.download(
+                output_path=output_path_entry.get(),
+                filename_prefix=itag)
+            print('Download Completed')
+
+            # TODO PROGRESSING STUFF
+
+
 root = tkinter.Tk()
-root.geometry("720x400")
+root.geometry("800x400")
 frame = tkinter.Frame(root)
 frame.pack()
 
 ## url label
 url_label = Label(frame, text="Video URL")
-url_label.grid(row=0, column=0, padx=8, pady=8)
+url_label.grid(row=0, column=0, padx=8, pady=4)
 
 ## URL entry
 url_entry = Entry(frame, width=64)
-url_entry.insert(0, '')
-url_entry.grid(row=0, column=1, padx=8, pady=8)
+url_entry.insert(0, 'https://www.youtube.com/watch?v=54_XRjHhZzI')
+url_entry.grid(row=0, column=1, padx=8, pady=4)
+
+## output path label
+output_path_label = Label(frame, text="Download path")
+output_path_label.grid(row=1, column=0, padx=8, pady=4)
+
+## output entry
+output_path_entry = Entry(frame, width=64)
+output_path_entry.insert(0,
+                         'C:\\Users\\IslamAlaaEddin\\PycharmProjects\\YoutubeDownloader\\Downloads')  # mst be 2 slashes
+output_path_entry.grid(row=1, column=1, padx=8, pady=4)
 
 ## search button
-search_button = Button(frame, text="Search", command=None)
-search_button.grid(row=0, column=3, padx=8, pady=8)
+search_button = Button(frame, text="Search", command=search_threaded)  # TODO MULTITHREADING
+search_button.grid(row=0, column=2, padx=8, pady=8)
+
+## download button
+download_button = Button(frame, text="Download", command=download_threaded)  # TODO MULTITHREADING
+download_button.grid(row=1, column=2, padx=8, pady=8)
 
 ## thumbnail
 img = ImageTk.PhotoImage(Image.open("jerry.jpg").resize((196, 196)))
@@ -27,28 +167,18 @@ panel = Label(frame, image=img, anchor="w")
 panel.grid(row=2, column=0, padx=8, pady=8)
 
 ## streams list box
-streams_listbox = tkinter.Listbox(frame)
-
-for i in range(100):
-    streams_listbox.insert(str(i), ' ------------------- ' + str(i + 1))
-
+streams_listbox = tkinter.Listbox(frame, selectmode="multiple", width=10, height=12)
 streams_listbox.grid(row=2, column=1, padx=8, pady=8, sticky='EW')
 
-# myscroll = Scrollbar(frame, orient='vertical')
-# myscroll.grid(row=2, column=1, sticky='ns')
-# myscroll.config(command = streams_listbox.yview)
-
-
-## download button
-download_button = Button(frame, text="Download", command=None)
-download_button.grid(row=3, column=1, padx=8, pady=8)
+# Progress bar widget
+download_progress_bar = Progressbar(frame, orient=HORIZONTAL,
+                                    length=400, mode='determinate')
+download_progress_bar.grid(row=3, column=1, padx=8, pady=8)
+download_progress_bar['value'] = 90  # percentage
 
 ## cancel downloading button
 cancel_downloading_button = Button(frame, text="Cancel", command=None)
 cancel_downloading_button.grid(row=3, column=2, padx=8, pady=8)
-
-
-
 
 root.title("Youtube Downloader")
 root.mainloop()
