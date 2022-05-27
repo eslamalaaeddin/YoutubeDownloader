32a6dc02e51b9a15cd178d611d41630de8decd6e
Islam AlaaEddin <islamalaaeddin1998@gmail.com>
2022-05-27 16:49:06 +0200

    Adding gui.py file and creating root window

---
+++ gui.py
@@ -0,0 +1,13 @@
+import tkinter
+from tkinter.ttk import Label, Entry, Button, Scrollbar
+
+from PIL import ImageTk, Image
+
+root = tkinter.Tk()
+root.geometry("720x400")
+frame = tkinter.Frame(root)
+frame.pack()
+
+
+root.title("Youtube Downloader")
+root.mainloop()
